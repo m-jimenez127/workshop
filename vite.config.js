3215fc67ff2eebc5f2aa416bd8fc1da2b177c3dd
Marco Jimenez <mjimenez.accur8@gmail.com>
2024-02-28 22:07:12 +0800

    Render on port 5174

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  test: {
+    globals: true,
+    environment: "jsdom",
+    exclude: ["./node_modules"],
+    setupFiles: "./src/test/setup.jsx",
+    testUrl: "http://localhost:5174/",
+    testEnvironment: "node",
+  },
+  server: {
+    port: 5174,
+  },
   plugins: [react()],
-})
+});
