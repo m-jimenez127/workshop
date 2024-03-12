ef462a5c6cc2c1b672deace785ba66fb18920c8d
Marco Jimenez <mjimenez.accur8@gmail.com>
2024-03-12 11:37:42 +0800

    Add Company Validator

--- src/utils/validator.jsx
+++ src/utils/validator.jsx
@@ -30,15 +30,60 @@ const validateProject = (data) => {
   if (data?.alias?.length < 1) {
     retData.isValid = false;
     retData.errors.alias = "Alias is required.";
-  }
-
-  const aliasRegex = /^\S+$/;
-  if (!aliasRegex.test(data.alias)) {
-    retData.isValid = false;
-    retData.errors.alias = "Alias should not contain any spaces.";
+  } else {
+    const aliasRegex = /^\S+$/;
+    if (!aliasRegex.test(data.alias)) {
+      retData.isValid = false;
+      retData.errors.alias = "Alias should not contain any spaces.";
+    }
   }
 
   return retData;
 };
 
-export { validateResource, validateProject };
+const validateCompany = (data) => {
+  const retData = { isValid: true, errors: {} };
+
+  if (data?.name?.length < 1) {
+    retData.isValid = false;
+    retData.errors.name = "Company Name is required.";
+  }
+
+  if (data?.address?.length < 1) {
+    retData.isValid = false;
+    retData.errors.address = "Company Address is required.";
+  }
+
+  if (data?.contactPerson?.length < 1) {
+    retData.isValid = false;
+    retData.errors.contactPerson = "Contact Person is required.";
+  }
+
+  if (data?.email?.length < 1) {
+    retData.isValid = false;
+    retData.errors.email = "Email is required.";
+  } else {
+    const emailRegex = new RegExp(/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/i);
+    if (!emailRegex.test(data.email)) {
+      retData.isValid = false;
+      retData.errors.email =
+        "Please input a valid Email Address (eg. juandelacruz@gmail.com).";
+    }
+  }
+
+  if (data?.contactNumber?.length < 1) {
+    retData.isValid = false;
+    retData.errors.contactNumber = "contactNumber is required.";
+  } else {
+    const contactNumberRegex = new RegExp(/^(09|\+639)\d{9}$/gm);
+    if (!contactNumberRegex.test(data.contactNumber)) {
+      retData.isValid = false;
+      retData.errors.contactNumber =
+        "Please input a valid Contact Number (eg. 09123456789 or +639123456789).";
+    }
+  }
+
+  return retData;
+};
+
+export { validateResource, validateProject, validateCompany };
